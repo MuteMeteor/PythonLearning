82f86a8e13aab1a897c5c3b163c8f0d7b512e4be
kyrenewong <kyrenewong@163.com>
2018-01-17 19:51:35 +0800

    20180117

---
+++ 01 Python3/helloworld.py
@@ -0,0 +1,40 @@
+#-*- coding:utf-8 -*-       python2
+
+# 单行注释
+
+# 多行注释
+'''
+print("hello world")
+print("hello world")
+'''
+
+"""
+print("hello world")
+print("hello world")
+"""
+
+# 1.打印hello world
+print("hello world")
+
+# 2.定义变量
+score = 100  
+
+# 3.输入
+age = input("please enter your age:")
+
+# 4.输出
+age = 18
+print("your age is %d"%age)
+name = "mutemeteor"
+print("the name is: %s"%name)
+
+# 5.if-else判断
+age = 20
+if age>18:
+    print("you are an adult")
+else:
+    print("you are not an adult")
+
+
+
+
