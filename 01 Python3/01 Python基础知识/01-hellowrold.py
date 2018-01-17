763a55ddbe3e6bb67257779bbde1f1066db78a3a
kyrenewong <kyrenewong@163.com>
2018-01-17 15:37:17 +0800

    20180117

---
+++ 01 Python3/01 Python基础知识/01-hellowrold.py
@@ -0,0 +1,19 @@
+#-*- coding:utf-8 -*-  python2
+
+
+#以下代码用来打印一个信息
+print("hello world")
+
+
+# 单行注释
+
+# 多行注释
+'''
+print("hello world")
+print("hello world")
+'''
+
+"""
+print("hello world")
+print("hello world")
+"""
