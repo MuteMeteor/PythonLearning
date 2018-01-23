9400b119e16be262a7fe61250f3f2f5946018607
kyrenewong <kyrenewong@163.com>
2018-01-23 18:23:44 +0800

    20180123

---
+++ 01 Python3/isPhoneNumber.py
@@ -0,0 +1,27 @@
+# 不用正则表达式来查找文本模式
+# 电话号码模式：3个数字-3个数字-4个数字
+# 例如：415-555-4242
+
+
+def isPhoneNumber(text):
+    if len(text) != 12:
+        return False
+    for i in range(0,3):
+        if not text[i].isdecimal:
+            return False
+    if text[3] != '-':
+        return False
+    for i in range(4,7):
+        if not text[i].isdecimal:
+            return False
+    if text[7] != '-':
+         return False
+    for i in range(9,12):
+        if not text[i].isdecimal:
+            return False
+    return True
+
+print('415-555-4242 is a phone number:')
+print(isPhoneNumber('415-555-4242'))
+print('Moshi moshi is a phone number:')
+print(isPhoneNumber('Moshi moshi'))
